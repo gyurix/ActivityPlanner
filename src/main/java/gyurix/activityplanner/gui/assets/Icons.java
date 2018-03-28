6ac116a8c3fda8d1fb5f4643653a44965f6940ff
gyurix <barathgy@gmail.com>
2018-03-28 11:09:19 +0200

    Added chat icons

--- src/main/java/gyurix/activityplanner/gui/assets/Icons.java
+++ src/main/java/gyurix/activityplanner/gui/assets/Icons.java
@@ -5,15 +5,19 @@ import javafx.scene.image.Image;
 public enum Icons {
     ADD("add.png"),
     BROWSE("browse.png"),
+    CHAT_BOTH("chat/both.png"),
+    CHAT_LECTOR("chat/lector.png"),
+    CHAT_STUDENT("chat/student.png"),
+    CHAT_STUDENTS("chat/students.png"),
     EDIT("edit.png"),
-    LOADING("loading.gif"),
-    LOGO("logo.png"),
-    REMOVE("remove.png"),
     ELEMENT_AUDIO("element/audio.png"),
     ELEMENT_PICTURE("element/picture.png"),
     ELEMENT_TEXT("element/text.png"),
     ELEMENT_URL("element/url.png"),
-    ELEMENT_VIDEO("element/video.png");
+    ELEMENT_VIDEO("element/video.png"),
+    LOADING("loading.gif"),
+    LOGO("logo.png"),
+    REMOVE("remove.png");
 
     private String fileName;
     private Image image;
--- src/main/java/gyurix/activityplanner/gui/scenes/main/UserScene.java
+++ src/main/java/gyurix/activityplanner/gui/scenes/main/UserScene.java
@@ -23,6 +23,7 @@ import static gyurix.activityplanner.gui.scenes.SceneUtils.*;
 
 @Getter
 public class UserScene extends InfoScene<User> {
+    private static final double CHAT_ICON_SIZE = 0.06;
     private static final double ICON_SIZE = 0.025;
     private static final Color alertBackground = Color.web("#ff7070");
     private static final Color chatBackground = Color.web("#a0a0ff");
@@ -43,21 +44,8 @@ public class UserScene extends InfoScene<User> {
 
     @Override
     public void addNodesToGrid() {
-        addNodesToMainGrid();
         addNodesToGridChat();
-    }
-
-    private void addNodesToMainGrid() {
-        grid.add(usernameLabel, 1, 0);
-        grid.add(logoutButton, 2, 0);
-        grid.add(doubleWrap(tableWrapper), 0, 1);
-        grid.add(doubleWrap(alertsWrapper), 1, 1);
-        grid.add(doubleWrap(chat), 2, 1);
-    }
-
-    @Override
-    public void createScene() {
-        createResizableScene(0.8, "User Dashboard");
+        addNodesToMainGrid();
     }
 
     @Override
@@ -68,6 +56,7 @@ public class UserScene extends InfoScene<User> {
             disable();
             new LoginScene(stage).start();
         });
+        chat.setBackground(bgColorGradient(chatBackground));
         attach(info.getCreatedContents(), () -> info.visitCreatedContents(renderer = new ContentRenderer(this)));
         alertsWrapper = createWrapper(alerts, bgColorGradient(alertBackground),
                 createClickableImage(Icons.ADD, ICON_SIZE, () -> {
@@ -91,26 +80,9 @@ public class UserScene extends InfoScene<User> {
         attach(un, () -> usernameLabel.setText(un.getData()));
     }
 
-    public void makeGridColumns() {
-        makeMainGridColumns();
-        ColumnConstraints side = new ColumnConstraints();
-        side.setPercentWidth(5);
-        ColumnConstraints main = new ColumnConstraints();
-        main.setPercentWidth(90);
-        tables.getColumnConstraints().addAll(side, main, side);
-        alerts.getColumnConstraints().addAll(side, main, side);
-    }
-
     @Override
-    public void makeGridRows() {
-        RowConstraints row0 = new RowConstraints();
-        RowConstraints row1 = new RowConstraints();
-        row0.setPercentHeight(10);
-        row1.setPercentHeight(90);
-        grid.getRowConstraints().addAll(row0, row1);
-    }
-
-    private void addNodesToGridChat() {
+    public void createScene() {
+        createResizableScene(0.8, "User Dashboard");
     }
 
     @Override
@@ -124,6 +96,45 @@ public class UserScene extends InfoScene<User> {
 
         makeGridColumns();
         makeGridRows();
+    }
+
+    public void makeGridColumns() {
+        makeChatGridColumns();
+        makeContentGridColumns();
+        makeMainGridColumns();
+    }
+
+    @Override
+    public void makeGridRows() {
+        RowConstraints row0 = new RowConstraints();
+        RowConstraints row1 = new RowConstraints();
+        row0.setPercentHeight(10);
+        row1.setPercentHeight(90);
+        grid.getRowConstraints().addAll(row0, row1);
+        chat.getRowConstraints().addAll(row0, row1);
+    }
+
+    private void addNodesToGridChat() {
+        chat.add(createClickableImage(Icons.CHAT_STUDENT, CHAT_ICON_SIZE, () -> {
+            System.out.println("Clicked to student");
+        }), 1, 0);
+        chat.add(createClickableImage(Icons.CHAT_STUDENTS, CHAT_ICON_SIZE, () -> {
+            System.out.println("Clicked to students");
+        }), 2, 0);
+        chat.add(createClickableImage(Icons.CHAT_LECTOR, CHAT_ICON_SIZE, () -> {
+            System.out.println("Clicked to lector");
+        }), 3, 0);
+        chat.add(createClickableImage(Icons.CHAT_BOTH, CHAT_ICON_SIZE, () -> {
+            System.out.println("Clicked to both");
+        }), 4, 0);
+    }
+
+    private void addNodesToMainGrid() {
+        grid.add(usernameLabel, 1, 0);
+        grid.add(logoutButton, 2, 0);
+        grid.add(doubleWrap(tableWrapper), 0, 1);
+        grid.add(doubleWrap(alertsWrapper), 1, 1);
+        grid.add(doubleWrap(chat), 2, 1);
     }
 
     private GridPane createWrapper(GridPane pane, Background bg, Pane addButton) {
@@ -184,11 +195,21 @@ public class UserScene extends InfoScene<User> {
         return col;
     }
 
-    public ColumnConstraints makeColumn(double width, HPos alignment) {
-        ColumnConstraints col = new ColumnConstraints();
-        col.setPercentWidth(width);
-        col.setHalignment(alignment);
-        return col;
+    private void makeChatGridColumns() {
+        ColumnConstraints side = new ColumnConstraints();
+        side.setPercentWidth(6);
+        ColumnConstraints body = new ColumnConstraints();
+        body.setPercentWidth(22);
+        chat.getColumnConstraints().addAll(side, body, body, body, body, side);
+    }
+
+    private void makeContentGridColumns() {
+        ColumnConstraints side = new ColumnConstraints();
+        side.setPercentWidth(5);
+        ColumnConstraints main = new ColumnConstraints();
+        main.setPercentWidth(90);
+        tables.getColumnConstraints().addAll(side, main, side);
+        alerts.getColumnConstraints().addAll(side, main, side);
     }
 
     public void makeMainGridColumns() {
