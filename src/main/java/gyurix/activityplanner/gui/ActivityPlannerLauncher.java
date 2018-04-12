a8523bf49992d80c9cda3b1054d3e061e8d65b37
gyurix <barathgy@gmail.com>
2018-04-12 09:16:54 +0200

    Added documentation to ActivityPlannerLauncher class

--- src/main/java/gyurix/activityplanner/gui/ActivityPlannerLauncher.java
+++ src/main/java/gyurix/activityplanner/gui/ActivityPlannerLauncher.java
@@ -9,14 +9,30 @@ import lombok.Getter;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 
+/**
+ * Launcher of the ActivityPlanner application.
+ */
 public class ActivityPlannerLauncher extends Application {
+    /**
+     * Instance of this launcher
+     */
     @Getter
     private static ActivityPlannerLauncher instance;
 
+    /**
+     * The main entry point of the application
+     *
+     * @param args - Running arguments (ignored)
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Starts the ActivityPlanner
+     *
+     * @param stage - Main application windows Stage, provided by JavaFX
+     */
     public void start(Stage stage) {
         instance = this;
         System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0");
